e663c8aad90bcada6fa4232a39d7f7e021ccb93e
WyLToR <bekesi.patrik@gmail.com>
2024-01-22 15:07:27 +0100

    update not found exception

--- src/utils/not-found.exception.ts
+++ src/utils/not-found.exception.ts
@@ -1,7 +1,7 @@
 import { NotFoundException } from '@nestjs/common';
 
 export class NotFoundDataException extends NotFoundException {
-  constructor(endpoint: string,id: number) {
+  constructor(endpoint: string,id: string) {
     super(`${endpoint} with id ${id} not found`);
   }
 }
