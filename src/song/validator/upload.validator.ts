914746883388f7531dd50f1d85d47197be8e1ca4
WyLToR <bekesi.patrik@gmail.com>
2024-01-24 14:41:27 +0100

    song upload validator

---
+++ src/song/validator/upload.validator.ts
@@ -0,0 +1,25 @@
+import { FileValidator } from '@nestjs/common';
+import { Express } from 'express'
+
+export interface CustomUploadTypeValidatorOptions {
+    fileType: string[];
+}
+
+export class CustomUploadFileTypeValidator extends FileValidator {
+    private _allowedMimeTypes: string[];
+
+    constructor(protected readonly validationOptions: CustomUploadTypeValidatorOptions) {
+        super(validationOptions);
+        this._allowedMimeTypes = this.validationOptions.fileType;
+    }
+
+    isValid(file?: Express.Multer.File): boolean {
+        return this._allowedMimeTypes.includes(file.mimetype);
+    }
+
+    buildErrorMessage(): string {
+        return `Upload not allowed. Upload only files of type: ${this._allowedMimeTypes.join(
+            ', ',
+        )}`;
+    }
+}
