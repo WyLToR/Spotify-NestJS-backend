9b994f794ffd0bec73cad06144a757c799d0a94c
WyLToR <bekesi.patrik@gmail.com>
2024-02-05 19:31:07 +0100

    add role to admin routes

--- src/admin/admin.controller.ts
+++ src/admin/admin.controller.ts
@@ -1,19 +1,26 @@
-import { Controller, Get, Param, Patch } from '@nestjs/common';
-import { ApiTags, ApiOperation, ApiResponse, ApiParam } from '@nestjs/swagger';
+import { Controller, Get, Param, Patch, UseGuards } from '@nestjs/common';
+import { ApiTags, ApiOperation, ApiResponse, ApiParam, ApiBearerAuth } from '@nestjs/swagger';
 import { AuthService } from '../auth/auth.service';
+import { JwtGuard } from '../auth/guard';
+import { RolesGuard } from '../auth/roles';
+import { Roles } from '../auth/decorators';
+import Role from '../utils/role.enum';
 
+@UseGuards(JwtGuard, RolesGuard)
+@ApiBearerAuth()
 @ApiTags('admin')
 @Controller('admin')
 export class AdminController {
-  constructor(private readonly authService: AuthService) {}
-
+  constructor(private readonly authService: AuthService) {
+  }
+  @Roles(Role.ADMIN)
   @ApiOperation({ summary: 'Get all users' })
   @ApiResponse({ status: 200, description: 'Return the list of all users' })
   @Get('/users')
   async getAllUsers() {
     return this.authService.getAllUser();
   }
-
+  @Roles(Role.ADMIN)
   @ApiOperation({ summary: 'Switch user role' })
   @ApiParam({ name: 'userId', description: 'ID of the user', type: 'string' })
   @ApiResponse({ status: 200, description: 'User role switched successfully' })
