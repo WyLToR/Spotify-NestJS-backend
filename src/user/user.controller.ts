93c55464adb86888dfaf66c587b4c25889233fbe
WyLToR <bekesi.patrik@gmail.com>
2024-02-06 14:26:42 +0100

    add dto, add image upload logic to patch route

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, UseGuards, Patch, Param, Body, Delete} from '@nestjs/common';
+import { Controller, UseGuards, Patch, Param, Body, Delete, UploadedFile, ParseFilePipeBuilder, HttpStatus, UseInterceptors} from '@nestjs/common';
 import { JwtGuard } from '../auth/guard';
 import { ApiOperation, ApiResponse, ApiTags, ApiBody, ApiBearerAuth } from '@nestjs/swagger';
 import { AuthService } from '../auth/auth.service';
@@ -6,6 +6,15 @@ import { AuthDto } from '../auth/dto';
 import { RolesGuard } from '../auth/roles';
 import { Roles } from '../auth/decorators';
 import { Role } from '@prisma/client';
+import { CustomUploadFileTypeValidator } from 'src/song/validator';
+import { UserDto } from './dto';
+import { FileInterceptor } from '@nestjs/platform-express';
+
+const MAX_PICTURE_SIZE = 5 * 1024 * 1024;
+const VALID_IMAGE_MIME_TYPES = [
+    'image/jpeg', 'image/png', 'image/gif', 'image/bmp',
+    'image/webp'
+];
 
 @UseGuards(JwtGuard, RolesGuard)
 @Controller('user')
@@ -15,16 +24,29 @@ export class UserController {
     @Patch("/:userId")
     @Roles(Role.USER, Role.ADMIN)
     @ApiBearerAuth()
+    @UseInterceptors(FileInterceptor('pictureFile'))
     @ApiOperation({ summary: 'Update User' })
     @ApiResponse({ status: 200, description: 'Return updated user data' })
     @ApiResponse({ status: 400, description: 'Invalid request or user not found' })
     @ApiBody({ type: AuthDto })
-    async updateUser(
+    updateUser(
+        @Body() dto: UserDto,
         @Param('userId') userId: string,
-        @Body() dto: AuthDto
+        @UploadedFile(
+            new ParseFilePipeBuilder()
+                .addValidator(
+                    new CustomUploadFileTypeValidator({
+                        fileType: VALID_IMAGE_MIME_TYPES,
+                    }),
+                )
+                .addMaxSizeValidator({ maxSize: MAX_PICTURE_SIZE })
+                .build({ errorHttpStatusCode: HttpStatus.UNPROCESSABLE_ENTITY })
+            ,
+        )
+        pictureFile: Express.Multer.File,
     ) {
         try {
-            return this.authService.updateUser(userId, dto);
+            return this.authService.updateUser(userId, dto, pictureFile);
         } catch (error) {
             return { error: error.message };
         }
