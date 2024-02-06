0ee0bbaee55eb90d38094d3d3607cf2ed2e2805d
WyLToR <bekesi.patrik@gmail.com>
2024-02-06 14:27:13 +0100

    album dto

--- src/album/dto/album.dto.ts
+++ src/album/dto/album.dto.ts
@@ -1,9 +1,13 @@
 import { ApiProperty } from "@nestjs/swagger";
-import { IsString, IsNotEmpty } from "class-validator";
+import { IsString, IsNotEmpty, IsOptional } from "class-validator";
 
 export class AlbumDto {
     @ApiProperty()
     @IsString()
     @IsNotEmpty()
     albumName: string;
+
+    @ApiProperty({ type: 'string', format: 'binary', required: false })
+    @IsOptional()
+    pictureFile?: Express.Multer.File;
 }
